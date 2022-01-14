cc99d6028546dd242fac2161c0f8eb7e0cce5b10
Luis Eduardo <67874424+The-Programador@users.noreply.github.com>
2022-01-14 00:29:39 -0300

    Update script.js
    Atualizações na lógica de mudança de tema claro para escuro (dynamicTheme())

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 let dateSystem = new Date();
 
-function load() {
+function load() { //Carrega os objetos e chama a função dinamicTheme()
     bodyPage = document.getElementById('body-page');
     main = document.getElementsByTagName('main');
     headerPage = document.getElementById('top');
@@ -13,24 +13,24 @@ function load() {
     dynamicTheme();
 }
 
-function dynamicTheme() {
-    if(dateSystem.getHours() >= 18)  {
+function dynamicTheme() { //Função muda tema do site
+    if(dateSystem.getHours() >= 18 || (dateSystem.getHours() < 6))  {
         main[0].style.padding = '0 4px 0';
         bodyPage.style.backgroundColor = '#222';
         headerPage.style.backgroundColor = '#111';
         headerPage.style.color = 'white';
-        headerPage.style.marginBottom = '0';
+        headerPage.style.marginBottom = '1px';
         headerPage.style.boxShadow = '0px 1px 3px #fff5';
         wrapper.style.backgroundColor = '#222';
         wrapper.style.border = '1px solid #fff7';
         footer.style.backgroundColor = '#fff';
-        textFooter[0].style.color = 'black';
+        textFooter[0].style.color = '#000';
         for(let cont = 0; cont < text.length; cont++) {
             text[cont].style.color = '#fff';//texto das sections
         }
 
         for(let cont = 0; cont < text.length; cont++) {
-            link[cont].style.color = '#DAA520'; //Cor links
+            link[cont].style.color = '#daa520'; //Cor links
         }
     }
 }
