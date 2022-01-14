ebe86d0a86f101dc686f5b689e3a4f4f58d213e6
Luis Eduardo <67874424+The-Programador@users.noreply.github.com>
2022-01-14 02:51:38 -0300

    Tema Dinâmico

--- script/script.js
+++ script/script.js
@@ -9,28 +9,46 @@ function load() { //Carrega os objetos e chama a função dinamicTheme()
     link = document.getElementsByClassName('link-basic');
     textFooter = document.getElementsByClassName('text-footer');
     footer = document.getElementById('footer');
-
-    dynamicTheme();
 }
 
-function dynamicTheme() { //Função muda tema do site
+function darkTheme() { //Função muda tema do site
     if(dateSystem.getHours() >= 18 || (dateSystem.getHours() < 6))  {
         main[0].style.padding = '0 4px 0';
-        bodyPage.style.backgroundColor = '#222';
-        headerPage.style.backgroundColor = '#111';
-        headerPage.style.color = 'white';
+        bodyPage.style.backgroundColor = 'var(--background-page-dark)';
+        headerPage.style.backgroundColor = 'var(--header-dark)';
         headerPage.style.marginBottom = '1px';
-        headerPage.style.boxShadow = '0px 1px 3px #fff5';
-        wrapper.style.backgroundColor = '#222';
-        wrapper.style.border = '1px solid #fff7';
-        footer.style.backgroundColor = '#fff';
+        headerPage.style.boxShadow = '0px 1px 3px var(--header-box-shadow)';
+        wrapper.style.backgroundColor = 'var(--background-page-dark)';
+        wrapper.style.border = '1px solid var(--wrapper-border-dark)';
+        footer.style.backgroundColor = 'var(--color1)';
         textFooter[0].style.color = '#000';
         for(let cont = 0; cont < text.length; cont++) {
-            text[cont].style.color = '#fff';//texto das sections
+            text[cont].style.color = 'var(--color1)';//texto das sections
         }
 
         for(let cont = 0; cont < text.length; cont++) {
-            link[cont].style.color = '#daa520'; //Cor links
+            link[cont].style.color = 'var(--color-link-dark)'; //Cor links
+        }
+    }
+}
+
+function lightTheme() { //Função muda tema do site
+    if(dateSystem.getHours() >= 18 || (dateSystem.getHours() < 6))  {
+        main[0].style.padding = '0';
+        bodyPage.style.backgroundColor = 'var(--background-page)';
+        headerPage.style.backgroundColor = 'var(--color2)';
+        headerPage.style.marginBottom = '0';
+        headerPage.style.boxShadow = 'none';
+        wrapper.style.backgroundColor = 'var(--color1)';
+        wrapper.style.border = 'none';
+        footer.style.backgroundColor = 'var(--color2)';
+        textFooter[0].style.color = 'var(--color1)';
+        for(let cont = 0; cont < text.length; cont++) {
+            text[cont].style.color = 'var(--color2)';//texto das sections
+        }
+
+        for(let cont = 0; cont < text.length; cont++) {
+            link[cont].style.color = 'var(--color1)'; //Cor links
         }
     }
 }
